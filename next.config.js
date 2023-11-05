82a90d4538e6211d1188ffa2e01c8905f8434e89
mazzampr <moh.azam121@gmail.com>
2023-11-05 16:34:06 +0700

    change config next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["localhost", "web-bem-testing.herokuapp.com"],
+    domains: ["localhost", "bemfasilkom-deploy.vercel.app"],
   },
   typescript: {
     ignoreBuildErrors: true,
