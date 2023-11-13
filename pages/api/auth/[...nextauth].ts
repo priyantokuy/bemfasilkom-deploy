bba6494b52a090349009ed925b7daf44040f191e
mazzampr <moh.azam121@gmail.com>
2023-11-13 08:09:27 +0700

    rollback auth redirect

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,17 +23,12 @@ export default NextAuth({
       return false;
     },
     redirect(url, baseUrl) {
-    
-    const customBaseUrl = process.env.NEXTAUTH_URL;
+      const customBaseUrl = process.env.NEXT_URL || "http://localhost:3000";
       
-      if (url.startsWith(`${baseUrl}/`)) {
-        return url;
-      } else if (url.startsWith("/")) {
-        return new URL(url, customBaseUrl).toString();
-      }
-      
-      // Handle the case where `url` is not a valid URL
-      return Promise.reject(new Error("Invalid URL"));
-    },
+    if (url.startsWith(baseUrl)) return url;
+    // Allows relative callback URLs
+    else if (url.startsWith("/")) return new URL(url, customBaseUrl).toString();
+    return customBaseUrl;
+  },
   },
 });
