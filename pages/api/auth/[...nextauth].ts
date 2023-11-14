a20d64fc78fea047cb09d13b213cb03fbf190e4a
mazzampr <moh.azam121@gmail.com>
2023-11-14 21:24:45 +0700

    edit redirect url 2

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,7 +23,7 @@ export default NextAuth({
       return false;
     },
     async redirect(url, baseUrl) {
-      const customBaseUrl = process.env.NEXT_URL || "https://bemfasilkomupnjatim.com";
+      const customBaseUrl = process.env.NEXTAUTH_URL || "https://bemfasilkomupnjatim.com";
       
     if (url.startsWith(baseUrl)) return url;
     // Allows relative callback URLs
